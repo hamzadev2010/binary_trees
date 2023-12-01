04c5acb0457f124403501fd09ddf2e1c68b47db4
hamza <hamzachanik@outlook.com>
2023-12-01 07:30:35 -0800

    task

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 /**
  * binary_tree_uncle - Script to find uncle o binary tree
- * @node: Pointer of the node 
- * Return: Uncle pointer 
+ * @node: Pointer of the node
+ * Return: Uncle pointer
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent ||
-	    !node->parent->parent )
+	    !node->parent->parent)
 	{
 		return (NULL);
 	}
