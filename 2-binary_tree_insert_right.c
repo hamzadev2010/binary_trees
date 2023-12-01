87de6cf3af1896366fb76bb066ba189ad07e1998
hamza <hamzachanik@outlook.com>
2023-12-01 01:45:11 -0800

    task

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserting.
+ * @parent: Inserting on the new node.
+ * @value: Value of the new node.
+ * Return: Creating new node. 
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+		return (NULL);
+	newnode = binary_tree_node(parent, value);
+	if (newnode == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		(parent->right)->parent = newnode;
+		newnode->right = parent->right;
+	}
+	parent->right = newnode;
+	return (newnode);
+}
