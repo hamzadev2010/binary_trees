a5b0644015bb8dbc1791da0912a572659c1c2a6a
hamza <hamzachanik@outlook.com>
2023-12-01 02:46:25 -0800

    task

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,7 +2,7 @@
 
 /**
   * binary_tree_inorder - Function in binary tree
-  * @tree: Tree root 
+  * @tree: Tree root
   * @func: Call function.
   */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
