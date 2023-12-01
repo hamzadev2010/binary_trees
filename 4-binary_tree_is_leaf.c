5720bf56a2a32c4329cbc111ca7e602eda2c4a88
hamza <hamzachanik@outlook.com>
2023-12-01 02:16:47 -0800

    task

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,15 +2,15 @@
 
 /**
  * binary_tree_is_leaf - Cheking.
- * @node: Cheking the node.
+ * @node: Cheking the node
  *
  * Return: 1 if success otherwise 0.
  */
-int binary_tree_is_leaf(const binary_tree_t *nod)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!nod)
+	if (!node)
 		return (0);
-	if (!(nod->left) && !(nod->right))
+	if (!(node->left) && !(node->right))
 		return (1);
 	return (0);
 }
